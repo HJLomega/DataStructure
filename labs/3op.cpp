d74aededadd970bc1f6a3aba4e48dabcdf45e68a
ljh <2450642014@qq.com>
2022-12-11 21:00:11 +0800

    false

--- labs/3op.cpp
+++ labs/3op.cpp
@@ -1,48 +1,56 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <queue>
 using namespace std;
 //树节点
 struct Node
 {
-	char value;
+	string value;
 	Node* left;
 	Node* right;
 };
-Node* exp2tree(string exp) {
-	//没有输入
-	if (exp =="") {
-		return NULL;
+Node* exp2tree(queue<string>& exp) {
+	Node* tree = new Node;
+	//解析左表达式
+	if (exp.front() == "(") {
+		exp.pop(); //去（
+		tree->left = exp2tree(exp);
+		exp.pop(); //去）
 	}
-	exp2tree(exp);//解析左操作数
+	else {
+		//叶子
+		Node* leaf = new Node;
+		leaf->left = NULL;
+		leaf->right = NULL;
+		leaf->value = exp.front(); exp.pop();
+		tree->left = leaf;
+	}
 	//解析操作符
-	exp2tree(exp);//解析操作数
+	tree->value = exp.front(); exp.pop();
+	//解析右表达式
+	if (exp.front() == "(") {
+		exp.pop(); //去（
+		tree->right = exp2tree(exp);
+		exp.pop(); //去）
+	}
+	else {
+		//叶子
+		Node* leaf = new Node;
+		leaf->left = NULL;
+		leaf->right = NULL;
+		leaf->value = exp.front(); exp.pop();
+		tree->right = leaf;
+	}
+	return tree;
 }
 
-//读数字的辅助函数
-void read_num(vector<string>& exp,char c) { //c为首字符
-	//处理数字
-	if (c >= '0' && c <= '9') { 
-		string temp(1, c);
-		char digit;
-		while (cin >> digit && digit >= '0' && digit <= '9') {
-			temp = temp + digit;
-		}
-		exp.push_back(temp);
-		read_num(exp, digit);//递归调用处理最后一个读入的字符
-	}
-	//处理符号
-	else{
-		string temp(1, c);
-		exp.push_back(temp);
-	}
-}
+
 
 
 int main() {
 	//词法分析
-	vector<string> exp;
+	queue<string> exp;
 	char c;
 	while (cin >> c) {
 		if (c == '=') {
@@ -50,15 +58,27 @@ int main() {
 		}
 		//读数字
 		if (c >= '0' && c <= '9') { 
-			read_num(exp,c);
+			string temp(1, c);
+			while (cin >> c && c >= '0' && c <= '9') {
+				temp += c;
+			}
+			exp.push(temp);
+			//由于表达式时正确的，数字后必是符号
+			if (c == '=') {
+				break;
+			}
+			string temp2(1, c);
+			exp.push(temp2);
 		}
 		//读符号
 		else {
 			string temp(1,c);
-			exp.push_back(temp);
+			exp.push(temp);
 		}
 
 	}
-	//
+	//建树
 	Node* tree;
+	tree = exp2tree(exp);
+	int a = 5;
 }
