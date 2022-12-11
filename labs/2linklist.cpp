6a3a827da92c86389d652ca39b7a6891f2b0809f
ljh <2450642014@qq.com>
2022-12-11 18:10:22 +0800

    2 finish

--- labs/2linklist.cpp
+++ labs/2linklist.cpp
@@ -2,68 +2,60 @@
 两个非降序链表的并集，例如将链表1->2->3 和 2->3->5 并为 1->2->3->5，只能输出结果，不能修改两个链表的数据。
 */
 #include <stdio.h>
+#include <list>
+using namespace std;
 
-struct Node
-{
-	int value;
-	Node* next;
-};
-
-class LinkList {
-public:
-	Node* head;// 头指针 head->next为第一个一个节点
-	Node* end;// 尾指针 end->next为最后一个节点
-	LinkList() {
-		head = new Node;
-		end = new Node;
-		head->next = NULL;
-		end->next = NULL;
-	}
-	bool push_back(int num) {
-		Node* temp = new Node;
-		temp->value = num;
-		temp->next = NULL;
-		if (head->next == NULL) { //空表时
-			end->next = temp;
-			head->next = temp;
-		}
-		else {
-			end->next->next = temp;
-			end->next = temp;
-		}
-		return true;
-	}
-	~LinkList() {
-		Node* p = head->next;
-		while (p != NULL) {
-			Node* temp = p;
-			p = p->next;
-			delete temp;
-		}
-	}
-	void display() {
-		Node* p = head->next;
-		while (p != NULL) {
-			printf("%d ", p->value);
-			p = p->next;
-		}
-		printf("\n");
-	}
-};
-
-int main() {
-	LinkList a;
-	LinkList b;
+int main(void) {
+	list<int> a;
+	list<int> b;
 	int num;
 	char c;
+	//读取输入
 	do {
 		scanf("%d", &num);
 		a.push_back(num);
 	} while ((c = getchar()) != '\n' && c != EOF);
-	a.display();
 	do {
 		scanf("%d", &num);
 		b.push_back(num);
 	} while ((c = getchar()) != '\n' && c != EOF);
-	b.display();
+
+	/*
+	 可以先对 a，b进行冒泡排序，使得算法更健壮
+	*/ 
+
+	//归并排序
+	list<int> m;
+	auto pa = a.begin();
+	auto pb = b.begin();
+	while (pa!=a.end()&& pb != b.end()) {
+		if (*pa < *pb){
+			m.push_back(*pa);
+			pa++;
+		}
+		else {
+			m.push_back(*pb);
+			pb++;
+		}
+	}
+	while (pa != a.end()) {
+		m.push_back(*pa);
+		pa++;
+	}
+	while (pb != b.end()) {
+		m.push_back(*pb);
+		pb++;
+	}
+	//去重输出
+	int pre = *m.begin();
+	printf("%d ", pre);
+	for (int n : m) {
+		if (n == pre) {
+			continue;
+		}
+		else {
+			printf("%d ", n);
+			pre = n;
+		}
+	}
 }
