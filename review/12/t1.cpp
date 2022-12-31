dfe9495be9cc0a4963200b779dce07c6a0e58be0
ljh <2450642014@qq.com>
2022-12-31 18:12:10 +0800

    t1 finish

--- review/12/t1.cpp
+++ review/12/t1.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 /* 
 对单链表中元素按直接插入方法进行排序，算法的C语言描述如下，
 其中L为指向链表头结点的指针。请在空白处填入合适的代码，以完成排序功能。
@@ -19,5 +21,24 @@ void InsertSort(Link L) {
     }
 }
 
+void test() {
+    const int N = 5;
+    int ar[N] = { 5,4,3,2,1 };
+    Link list = nullptr;
+    //list->data = -1; list->next = nullptr;
+    for (int i = N - 1; i >= 0; i--) {
+        Link temp = new Linknode();
+        temp->data = ar[i]; temp->next = list;
+        list = temp;
+    }
+    Link temp = new Linknode();
+    temp->data = -1; temp->next = list;
+    list = temp;
 
+    InsertSort(list);
+    int b = 1;
+}
+int main() {
+    test();
+}
 
